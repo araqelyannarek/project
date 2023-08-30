1dc9a71a7dd813621cc92aab3e0198c71fc4b87c
araqelyannarek <araqelyann315@gmail.com>
2023-08-31 02:12:29 +0400

    test commit

--- front-end/components/signIn/SignInPage.tsx
+++ front-end/components/signIn/SignInPage.tsx
@@ -117,7 +117,7 @@ const SignIn = () => {
                                 color={'white'}
                                 _hover={{ bg: '#3D7DD6' }}
                              >
-                                Sign innnnnnnnnnn
+                                Sign innnnnnnnnnnnn
                             </Button>
                         </Stack>     
                         <Text align={'center'} color={'red.500'}>{errorMessage}</Text>
